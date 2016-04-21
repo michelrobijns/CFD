654f59e922901a8849a65f8fdddde2d18f20d71e
michelrobijns <michel.robijns@icloud.com>
2016-04-21 13:06:09 +0200

    Updating plotting routine

--- Code/C_ATLAS_opt/plotResults.py
+++ Code/C_ATLAS_opt/plotResults.py
@@ -18,19 +18,21 @@ def plotStreamFunctionContour():
     X = np.loadtxt('streamFunctionX.dat')
     Y = np.loadtxt('streamFunctionY.dat')
     
-    streamFunction = scipy.ndimage.zoom(streamFunction, 3)
-    X = scipy.ndimage.zoom(X, 3)
-    Y = scipy.ndimage.zoom(Y, 3)
+    streamFunction = scipy.ndimage.zoom(streamFunction, 1)
+    X = scipy.ndimage.zoom(X, 1)
+    Y = scipy.ndimage.zoom(Y, 1)
     
     levels = [-1.5e-3, -1e-3, -5e-4, -2.5e-4, -1e-4, -5e-5, -1e-5, -1e-6, 0,
               1e-10, 1e-5, 1e-4, 1e-2, 3e-2, 5e-2, 7e-2, 9e-2, 0.1, 0.11,
               0.115, 0.1175]
     
-    plt.figure(num='Stream function', figsize=(12, 12))
+    plt.figure(num='Stream function', figsize=(8, 8))
     plt.contour(X, Y, streamFunction, levels=levels, colors='k')
     frame = plt.gca()
     frame.axes.xaxis.set_ticklabels([])
     frame.axes.yaxis.set_ticklabels([])
+    frame.xaxis.set_major_locator(plt.NullLocator())
+    frame.yaxis.set_major_locator(plt.NullLocator())
     plt.savefig('streamFunction.pdf', format='pdf', bbox_inches='tight')
     plt.show()
 
@@ -39,17 +41,19 @@ def plotVorticityContour():
     X = np.loadtxt('vorticityX.dat')
     Y = np.loadtxt('vorticityY.dat')
     
-    vorticity = scipy.ndimage.zoom(vorticity, 3)
-    X = scipy.ndimage.zoom(X, 3)
-    Y = scipy.ndimage.zoom(Y, 3)
+    vorticity = scipy.ndimage.zoom(vorticity, 1)
+    X = scipy.ndimage.zoom(X, 1)
+    Y = scipy.ndimage.zoom(Y, 1)
     
     levels = [-3.0, -2.0, -1.0, -0.5, 0.0, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0]
     
-    plt.figure(num='Vorticity', figsize=(12, 12))
+    plt.figure(num='Vorticity', figsize=(8, 8))
     plt.contour(X, Y, vorticity, levels=levels, colors='k')
     frame = plt.gca()
     frame.axes.xaxis.set_ticklabels([])
     frame.axes.yaxis.set_ticklabels([])
+    frame.xaxis.set_major_locator(plt.NullLocator())
+    frame.yaxis.set_major_locator(plt.NullLocator())
     plt.savefig('vorticity.pdf', format='pdf', bbox_inches='tight')
     plt.show()
 
@@ -58,17 +62,19 @@ def plotPressureContour():
     X = np.loadtxt('pressureX.dat')
     Y = np.loadtxt('pressureY.dat')
     
-    pressure = scipy.ndimage.zoom(pressure, 3)
-    X = scipy.ndimage.zoom(X, 3)
-    Y = scipy.ndimage.zoom(Y, 3)
+    pressure = scipy.ndimage.zoom(pressure, 1)
+    X = scipy.ndimage.zoom(X, 1)
+    Y = scipy.ndimage.zoom(Y, 1)
     
     levels = [-0.002, 0, 0.02, 0.05, 0.07, 0.09, 0.11, 0.12, 0.17, 0.3]
     
-    plt.figure(num='Pressure', figsize=(12, 12))
+    plt.figure(num='Pressure', figsize=(8, 8))
     plt.contour(X, Y, pressure, levels=levels, colors='k')
     frame = plt.gca()
     frame.axes.xaxis.set_ticklabels([])
     frame.axes.yaxis.set_ticklabels([])
+    frame.xaxis.set_major_locator(plt.NullLocator())
+    frame.yaxis.set_major_locator(plt.NullLocator())
     plt.savefig('pressure.pdf', format='pdf', bbox_inches='tight')
     plt.show()
 
